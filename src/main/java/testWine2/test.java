351477eb5ccf4e4ada5d23bc0cbd2fb49698c23f
jinyoung0323 <kjycjb686@naver.com>
2021-04-24 16:41:45 +0900

    commits of kjy0323

--- src/main/java/testWine2/test.java
+++ src/main/java/testWine2/test.java
@@ -3,8 +3,9 @@ package testWine2;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("제발 좀 되라!!");
+		System.out.println("�젣諛� 醫� �릺�씪!!");
 		System.out.println("!!!!");
+		System.out.println("Hello, World!!!");
 	}
 
 }
